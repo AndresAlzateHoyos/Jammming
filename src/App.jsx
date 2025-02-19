d2859cf2ad3ebab3c6b7c580b97c3e99edbe29bc
AndresAlzate <andresalzate.trad@gmail.com>
2025-02-19 17:50:37 -0500

    Show only results and not all songs list

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,13 @@ import SearchResults from './SearchResults';
 
 function App () {
   const songs = ['I love you', 'Your name is', 'Beetlejuice', 'Paula Arango', 'Baby Come Back', 'Kimi Ma No Wa', 'Taxi Driver', 'This is the Lolcow Story'];
-  const [filteredResults, setFilteredResults] = useState(songs);
+  const [filteredResults, setFilteredResults] = useState([]);
 
   const handleSearch = query => {
+    if(!query) {
+      setFilteredResults([]);
+      return;
+    }
     const filtered = songs.filter(song => 
       song.toLowerCase().includes(query.toLowerCase())
     );
--- src/SearchResults.jsx
+++ src/SearchResults.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 
 function SearchResults({results}) {
+    if(results.length === 0) return null;
     return (
         <ul >
         {results.map((el, index) => (
